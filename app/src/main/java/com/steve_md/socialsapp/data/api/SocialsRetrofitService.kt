8f8e41cd4940c8cfed848c0c0dd71214f8572b9f
MuindiStephen <stephenmuindi241@gmail.com>
2023-04-05 18:45:00 +0300

    code [refactors]

--- app/src/main/java/com/steve_md/socialsapp/data/api/SocialsRetrofitService.kt
+++ app/src/main/java/com/steve_md/socialsapp/data/api/SocialsRetrofitService.kt
@@ -4,6 +4,6 @@ import com.steve_md.socialsapp.model.Posts
 import retrofit2.http.GET
 
 interface SocialsRetrofitService {
-    @GET("")
+    @GET("/photos")
     suspend fun getAllPosts() : List<Posts>
 }
