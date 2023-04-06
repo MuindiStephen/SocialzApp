3e90f5dfa2408a6ac9cf6f0b195d8cd68dd0ca82
MuindiStephen <stephenmuindi24@gmail.com>
2023-04-06 14:24:44 +0300

    JDK [version upgrade (#1)]

--- app/src/main/java/com/steve_md/socialsapp/ui/theme/Shape.kt
+++ app/src/main/java/com/steve_md/socialsapp/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-kpackage com.steve_md.socialsapp.ui.theme
+package com.steve_md.socialsapp.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
