138b17dfa403127a6715edf3ccbdb3da6539a1a0
MuindiStephen <stephenmuindi24@gmail.com>
2023-04-08 16:25:55 +0300

    build [APK]

--- app/src/main/java/com/steve_md/socialsapp/commons/utils/Constants.kt
+++ app/src/main/java/com/steve_md/socialsapp/commons/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.steve_md.socialsapp.commons.utils
 
 object Constants {
-    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    const val BASE_URL = "https://jsonplaceholder.typicode.com"
 }
